c2bbd92297b6c450e93da2c31bc78dbf9e7851d5
Michael J. Simons <michael@simons.ac>
2018-02-19 21:10:16 +0100

    Prepare directory structure

---
+++ faucet-pipeline-spring-boot-autoconfigure/src/main/java/org/faucetpipeline/faucetpipelinespringbootautoconfigure/FaucetPipelineSpringBootAutoconfigureApplication.java
@@ -0,0 +1,12 @@
+package org.faucetpipeline.faucetpipelinespringbootautoconfigure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaucetPipelineSpringBootAutoconfigureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaucetPipelineSpringBootAutoconfigureApplication.class, args);
+	}
+}
---
+++ faucet-pipeline-spring-boot-starter/src/main/java/org/faucetpipeline/faucetpipelinespringbootstarter/FaucetPipelineSpringBootStarterApplication.java
@@ -0,0 +1,12 @@
+package org.faucetpipeline.faucetpipelinespringbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaucetPipelineSpringBootStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaucetPipelineSpringBootStarterApplication.class, args);
+	}
+}
