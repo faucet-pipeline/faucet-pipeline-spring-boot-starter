99a071d90fce54f7c2f86d75efd02e398069b80f
Michael J. Simons <michael@simons.ac>
2018-02-20 19:49:53 +0100

    Scribble some more ideas

--- faucet-pipeline-spring-boot-autoconfigure/src/main/java/org/faucet_pipeline/spring/autoconfigure/FaucetPipelineAutoconfiguration.java
+++ faucet-pipeline-spring-boot-autoconfigure/src/main/java/org/faucet_pipeline/spring/autoconfigure/FaucetPipelineAutoconfiguration.java
@@ -1,14 +1,28 @@
 package org.faucet_pipeline.spring.autoconfigure;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 
 /**
  * @author Michael J. Simons, 2018-02-19
  */
 @Configuration
-@EnableConfigurationProperties(FaucetPipelineAutoconfiguration.class)
+@Conditional(AtLeastOneManifestExistsCondition.class)
+@ConditionalOnClass(ObjectMapper.class)
+@EnableConfigurationProperties(FaucetPipelineProperties.class)
 public class FaucetPipelineAutoconfiguration {
+
+    @Bean
+    public FaucetManifestHolder faucetManifestHolder(final FaucetPipelineProperties faucetPipelineProperties) {
+        for(Resource r : faucetPipelineProperties.getManifests())
+            System.out.println("r " + r);
+        return new FaucetManifestHolder();
+    }
 }
 
 /*
--- faucet-pipeline-spring-boot-autoconfigure/src/main/java/org/faucet_pipeline/spring/autoconfigure/FaucetPipelineProperties.java
+++ faucet-pipeline-spring-boot-autoconfigure/src/main/java/org/faucet_pipeline/spring/autoconfigure/FaucetPipelineProperties.java
@@ -1,10 +1,31 @@
 package org.faucet_pipeline.spring.autoconfigure;
 
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import static org.faucet_pipeline.spring.autoconfigure.FaucetPipelineProperties.PROPERTIES_PREFIX;
 
 /**
  * @author Michael J. Simons
  */
-@ConfigurationProperties(prefix = "faucet-pipeline")
+@ConfigurationProperties(prefix = PROPERTIES_PREFIX)
 public class FaucetPipelineProperties {
+
+    static final String PROPERTIES_PREFIX = "faucet-pipeline";
+    /**
+     * Path or resource for Faucets manifest. Defaults to
+     * <pre>manifest.json</pre>.
+     */
+    private List<Resource> manifests = Arrays.asList(new ClassPathResource("manifest.json"));
+
+    public List<Resource> getManifests() {
+        return manifests;
+    }
+
+    public void setManifests(List<Resource> manifests) {
+        this.manifests = manifests;
+    }
 }
