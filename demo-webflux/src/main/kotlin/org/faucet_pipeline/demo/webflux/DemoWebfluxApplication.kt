35a88487717253eb0bad4a25066f17d12cb7b4fa
Michael J. Simons <michael@simons.ac>
2018-03-04 11:50:54 +0100

    Add header

--- demo-webflux/src/main/kotlin/org/faucet_pipeline/demo/webflux/DemoWebfluxApplication.kt
+++ demo-webflux/src/main/kotlin/org/faucet_pipeline/demo/webflux/DemoWebfluxApplication.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.faucet_pipeline.demo.webflux
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
@@ -14,6 +29,14 @@ import org.springframework.web.reactive.function.server.ServerResponse.ok
 import org.springframework.web.reactive.function.server.router
 import org.springframework.web.reactive.resource.ResourceUrlProvider
 import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable
+
+/**
+ * Entry point to the idea application.
+ *
+ * @author Michael J. Simons
+ */
+@SpringBootApplication
+class DemoWebfluxApplication
 
 class UIHandler(val ideaRepository: IdeaRepository) {
 
@@ -40,9 +63,6 @@ fun beans() = beans {
         Router(ref()).routes()
     }
 }
-
-@SpringBootApplication
-class DemoWebfluxApplication
 
 fun main(args: Array<String>) {
     SpringApplicationBuilder()
